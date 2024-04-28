4077220b0003c100e78511df07558064fcc0ae52
Romulo Rosa <romulorosadev@gmail.com>
2024-04-28 19:36:34 -0300

    comment about variables

--- FormLogica/FormLogica/Program.cs
+++ FormLogica/FormLogica/Program.cs
@@ -13,14 +13,34 @@ namespace FormLogica
 
         // static | Informa ser uma função estática
         // void   | Informa ser também uma função sem retorno
-        // Main() | Método/função nativa do C#
-        // Main() | Como método principal é o primeiro a ser executado num programa em C#
-        // Main() | O bloco de código dentro de seu escopo é o primeiro a ser executado
-        // (string[] args) | Informa que dentro desse metódo temos textos 
+        // Main   | Método/função nativa do C#
+        // Main   | Como método principal é o primeiro a ser executado num programa em C#
+        // (string[] args) | Informa que dentro desse metódo temos dados do tipo string/textos 
+        // {...}  | Escopo no qual deve ser inserido o bloco de código referente a ser executado pelo método
         static void Main(string[] args)
         {
-            //Bloco de Código do Main
-            Console.WriteLine("Hello World!");
+            //Bloco dde Código do Método Main()
+
+
+            // DECLARAR VARIÁVEIS
+            // Tipo da variavel   | string
+            // Nome para variavel | inNome
+            // Fim da instrução   | ;
+            // tipo nomevariavel fimdainstrução | string inNome;
+
+
+            // DECLARAR VARIÁVEIS E INICIALIZAR AO RECEBER UM VALOR DE ATRIBUIÇÃO
+            // Tipo da variavel    | string
+            // Nome para variavel  | inNome
+            // Sinal de Atribuição | =
+            // Valor               | "Romulo"
+            // Fim da instrução    | ;
+            // tipo nomevariavel atribuição valor/operação/função fimdainstrução | string inNome = "Rosadev";
+
+            // Declaramos e inicializamos uma variavel pra armazenar e/ou manipular dados
+            string inNome = "Romulo";
+
+
         }
     }
 }
